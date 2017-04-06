55dd5a5e88ba8d6309e178efb34064b8849ffb79
caulax <caulax@gmail.com>
2017-04-06 21:48:06 +0300

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,32 @@
+<div class="menu-block">
+	<div class="menu-content">
+		<ul id="menu-main_menu" class="menu">
+			<li id="menu-item-7" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-5 current_page_item menu-item-7">
+				<a href="http://first-proj:5555/">Главная</a>
+			</li>
+			<li id="menu-item-9" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-9">
+				<a href="http://first-proj:5555/sample-page/">Пример страницы</a>
+				<ul class="sub-menu">
+					<li id="menu-item-13" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-13">
+						<a href="http://first-proj:5555/sample-page/">Пример страницы</a>
+					</li>
+					<li id="menu-item-14" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-14">
+						<a href="http://first-proj:5555/sample-page/">Пример страницы</a>
+					</li>
+					<li id="menu-item-15" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-15">
+						<a href="http://first-proj:5555/sample-page/">Пример страницы</a>
+					</li>
+				</ul>
+			</li>
+			<li id="menu-item-10" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-10">
+				<a href="http://first-proj:5555/sample-page/">Пример страницы</a>
+			</li>
+			<li id="menu-item-11" class="menu-item menu-item-type-post_type menu-item-object-post menu-item-11">
+				<a href="http://first-proj:5555/2017/04/04/privet-mir/">Привет, мир!</a>
+			</li>
+			<li id="menu-item-12" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-12">
+				<a href="http://leet.com.ua">Контакты</a>
+			</li>
+		</ul>
+	</div>
+</div> 
